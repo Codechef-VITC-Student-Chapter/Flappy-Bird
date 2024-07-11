568601b802a6117921345cdd10268f008c40dd52
Srilaya-sriraman <srilaya.sriraman2022@vitstudent.ac.in>
2024-07-11 13:19:40 +0530

    added homepage

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "flappybird-desk": "url('./assets/homePage_desktopBg.png')",
+        "flappybird-mob": "url('./assets/homePage_mobileBg.png')",
+      },
+    },
   },
   plugins: [],
 };
