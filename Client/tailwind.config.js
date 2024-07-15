544ec387bc87c47df4612828e366efb508bf3c61
Sushindh <146247085+Sushindh@users.noreply.github.com>
2024-07-15 23:35:53 +0530

    Update tailwind.config.js

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  theme: {
-    extend: {},
-  },
+  extend: {
+      screens:{
+        'max-w-600px':{'max':'600px'},
+        'min-w-600px':{'min':'600px'},
+      },
+      fontFamily: {
+        'fonts': ['"Post No Bills Colombo ExtraBold"','sans-serif'],
+        'fam':['"Post No Bills Jaffna ExtraBold"','sans-serif'],
+      }
+    },
   plugins: [],
 };
